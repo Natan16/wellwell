7d66b8895001e90360f2054c63d837111c6097b1
Natan16 <natanvianat16@gmail.com>
2024-01-31 10:45:03 -0300

    Basic functionality of search endpoint

--- wellwellb/app/controllers/WellnessController.scala
+++ wellwellb/app/controllers/WellnessController.scala
@@ -4,15 +4,15 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 import play.api.libs.ws._
-import scala.concurrent.Future
+import scala.concurrent.Future  
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class WellnessController @Inject()(ws: WSClient, val controllerComponents: ControllerComponents) extends BaseController {
-
+class WellnessController @Inject()(config: Configuration, ws: WSClient, val controllerComponents: ControllerComponents) extends BaseController {
+  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
   /**
    * Create an Action to render an HTML page.
    *
@@ -24,33 +24,17 @@ class WellnessController @Inject()(ws: WSClient, val controllerComponents: Contr
     Ok(views.html.index())
   }
 
-  def search(term: String) = Action { implicit request: Request[AnyContent] =>
-    val API_KEY = sys.env("VAGALUME_API_KEY")
+  def search(term: String) = Action.async { 
+    implicit request: Request[AnyContent] =>
+    val API_KEY = config.get[String]("VAGALUME_API_KEY")
     val BASE_URL = "https://api.vagalume.com.br/"
     val url = s"${BASE_URL}search.excerpt"
     val request : WSRequest = ws.url(url).withQueryString(
-      "q" -> term, "limit" -> "5"
+      "q" -> term, "limit" -> "100"
     ).withHeaders("Authorization" -> ("Bearer " + API_KEY))
-    val futureResponse : Future[WSResponse] = request.get()
-    // now parse the response and render the page
-    // https://api.vagalume.com.br/search.excerpt?q=vamos%20fugir&limit=5
-    
-    // calls API
-    // renders page with response
-    // is there any way to do this asynchronously and
-    // without re-rendering the whole page?
-    Ok(views.html.index()) // retornar um json que o twirl possa usar pra mostrar o resultado bonitinho
+    val futureResponse : Future[WSResponse] = request.get() 
+    futureResponse.map(response => Ok(
+      views.html.search(response.json("response")("docs").toString))
+    )
   }
 }
-
-// responseContém todas informações das músicas no Vagalume:
-// numFoundTotal de registros encontrados
-// startInício da busca (campo inalterável)
-// docsListagem dos registros encontrados
-// idID da música encontrada
-// urlA URL da música no Vagalume
-// titleO título da música encontrada
-// bandNome da banda
-// highlightingInformações sobre trechos encontrados.
-// l3ade68b7g3be34ea3O 1º caractere "l" refere-se á palavra "letra" e o restante ao ID da música
-// title/letraO texto da letra ou titulo da música, será destacado por "<em>...trecho...<em>".
